7cf0cbaf912c7e2fa1096b45c9dd15bd3c31398f
Wondi-Ke <wondifrawterefe7@gmail.com>
2025-04-14 17:12:44 +0300

    documentation update

--- social_media_app/urls.py
+++ social_media_app/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
+from drf_yasg.views import get_schema_view as view
 from drf_yasg import openapi
 
     
 # API schema view setup for a social media app
-schema_view = get_schema_view(
+schema_view = view(
    openapi.Info(
       title="Social Media API",
       default_version='v1',
@@ -36,10 +36,14 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/', include('posts.urls')),
-    path('api/', include('interactions.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('',
+         include([
+           path('api/', include('users.urls')),
+           path('api/', include('posts.urls')),
+           path('api/', include('interactions.urls')),
+           path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+           path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+           path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+         ])
+         )
 ]
